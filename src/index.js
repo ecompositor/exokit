0ced5681ae9b1a7db4c515ac74894e4339afb183
Avaer Kazmer <a@modules.io>
2019-04-30 20:21:57 -0400

    Initial zed SDK integration bootstrap

--- src/index.js
+++ src/index.js
@@ -934,6 +934,9 @@ const mlPresentState = {
 };
 GlobalContext.mlPresentState = mlPresentState;
 
+const zedContext = new nativeBindings.nativeZed();
+zedContext.RequestPresent();
+
 if (nativeBindings.nativeMl) {
   mlPresentState.mlContext = new nativeBindings.nativeMl();
   nativeBindings.nativeMl.requestPresent = function(layers) {
